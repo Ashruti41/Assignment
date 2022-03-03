ae1ce3bdbe3b96093cc084547d8ffd671aff0885
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-03-03 16:08:49 +0530

    Add files via upload

---
+++ c/array.c
@@ -0,0 +1,34 @@
+//check given element is in array or not and also print the array
+
+#include<stdio.h>
+
+int main()
+{
+    int arr[10]={1,5,4,8,9,2,0,6,11,7};
+    int size,i,search,flag;
+ 
+    printf("Enter the search element: ");
+    scanf("%d",&search);
+    flag = 0;
+    for(i=0; i<10;i++)
+    {
+        if(arr[i]==search)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    printf("\nElements in array is:");
+    for(i=0; i<10;i++)
+    {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
